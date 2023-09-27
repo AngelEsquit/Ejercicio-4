734f111b066ee16ba12bde6341775df5d9471732
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-26 20:14:10 -0600

    Update Portero.java

--- Portero.java
+++ Portero.java
@@ -1,3 +1,20 @@
 public class Portero extends Jugador {
-    
+    private int paradas_e;
+    private int goles_r;
+
+    public void setParadas_e(int paradas_e) {
+        this.paradas_e = paradas_e;
+    }
+
+    public int getParadas_e() {
+        return paradas_e;
+    }
+
+    public void setGoles_r(int goles_r) {
+        this.goles_r = goles_r;
+    }
+
+    public int getGoles_r() {
+        return goles_r;
+    }
 }
