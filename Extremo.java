9ea98dd9184f6d96ff8aad3ea6a2d6369b628634
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-26 20:15:02 -0600

    Update Extremo.java

--- Extremo.java
+++ Extremo.java
@@ -1,3 +1,20 @@
 public class Extremo extends Jugador {
-    
+    private int pases;
+    private int asistencias_e;
+
+    public void setPases(int pases) {
+        this.pases = pases;
+    }
+
+    public int getPases() {
+        return pases;
+    }
+
+    public void setAsistencias_e(int asistencias_e) {
+        this.asistencias_e = asistencias_e;
+    }
+
+    public int getAsistencias_e() {
+        return asistencias_e;
+    }
 }
