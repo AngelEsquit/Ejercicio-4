2508c090eaa5185762d8a1e499c20679734c92a9
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-26 20:58:16 -0600

    Update Extremo.java

--- Extremo.java
+++ Extremo.java
@@ -3,6 +3,17 @@ public class Extremo extends Jugador {
     private int asistencias_e;
     private Float efectividad;
 
+    public Extremo(String nombre, String pais, int faltas, int goles_d, int total_lanzamientos, int pases, int asistencias_e) {
+        super.setNombre(nombre);
+        super.setPais(pais);
+        super.setFaltas(faltas);
+        super.setGoles_d(goles_d);
+        super.setTotal_lanzamientos(total_lanzamientos);
+        this.pases = pases;
+        this.asistencias_e = asistencias_e;
+        this.efectividad = (float) ((((pases + asistencias_e - faltas) * 100) / (pases + asistencias_e + faltas)) + ((goles_d * 100) / total_lanzamientos));
+    }
+
     public void setPases(int pases) {
         this.pases = pases;
     }
